2c7c97d2949f4b6d3ee3fe265632292c41a8bd2b
Justin Maguire <maguirejustin@hotmail.com>
2015-03-17 21:56:49 -0400

    fix for incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,7 +6,7 @@ class IncomingController < ApplicationController
     
      user = User.find_by_email(params[:sender])
       if user
-        topic = Topic.find_or_create_by(params[:subject])
+        topic = Topic.find_or_create_by(title: params[:subject])
         #bookmark = Bookmark.find_or_create_by(url: params["body-plain"])
         bookmark = user.bookmarks.find_or_build_by(url: params["body-plain"])
         bookmark.topic = topic
